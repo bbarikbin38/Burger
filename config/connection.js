83e38f813579f273894887556bf72e1ca0cd8086
bbarikbin38 <bijan.barikbin38@gmail.com>
2019-05-13 20:06:24 -0400

    heroku connection updates

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ var mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connectin = mysql.createConnection ({
-        host: 'localhost',
-        user: 'root',
-        password:'hacktheplanet',
-        database: 'todoagain_db'
+    connection = mysql.createConnection ({
+        host: 'zf4nk2bcqjvif4in.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'ab417ofalhd80ksz',
+        password:'qqt35aj3puu4aez8',
+        database: 'byetd5qbuef74f8m'
     });
 };
 
